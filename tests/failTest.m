5c29fee3b41f022255f3378e4381c8fffb6a7ec3
sameagen <sameagen@mathworks.com>
2023-06-01 10:06:35 -0400

    Added more tests!

---
+++ tests/failTest.m
@@ -0,0 +1,11 @@
+function tests = failTest
+tests = functiontests(localfunctions);
+end
+
+function testSuccess(testCase)
+testCase.verifyTrue(true);
+end
+
+function testFailure(testCase)
+testCase.verifyTrue(false);
+end
